625a04cf094620afc2b07d28609d9f9c60db2b5e
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-10-31 04:59:54 -0400

    🐙 main.py
    🐙 main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Standard library imports
+import sys
+
+# Local application imports
+from game_logic import run_game
+
+def main():
+    """Entry point for the Q*bert game."""
+    run_game()
+
+if __name__ == "__main__":
+    main()
