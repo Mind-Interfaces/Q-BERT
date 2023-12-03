7a2324947eb85a9ba461d2d8beca055ec9c6e945
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-03 09:02:05 -0500

    🐙 rendering.py

--- rendering.py
+++ rendering.py
@@ -1,41 +1,43 @@
-# rendering.py
+import pygame
 
 class GameBoard:
-    """Handles the graphical rendering of the Q*bert game."""
-    
     def __init__(self):
-        """Initialize the game board."""
-        # For demonstration, initializing an empty 5x5 board
-        self.board = [[None for _ in range(5)] for _ in range(5)]
+        """Initialize the game board with Pygame."""
+        pygame.init()
+        self.screen_size = (1280, 720)
+        self.screen = pygame.display.set_mode(self.screen_size)
+        pygame.display.set_caption('Q*BERT\'S QUBES')
+        self.clock = pygame.time.Clock()
+        self.board_size = 5  # Example size, can be adjusted
+        self.tile_width = 50  # Width of each tile, adjust as needed
+        self.board = [[None for _ in range(self.board_size)] for _ in range(self.board_size)]
 
     def render(self, qbert, coily):
-        """Render the game board along with Q*bert and Coily.
-        
-        Parameters:
-            qbert (Qbert): The Q*bert character object.
-            coily (Coily): The Coily character object.
-        """
+        """Render the game board along with Q*bert and Coily."""
+        self.screen.fill((0, 0, 0))  # Clear screen with black background
         self.clear_board()
         self.place_character(qbert)
         self.place_character(coily)
         self.display_board()
+        pygame.display.flip()  # Update the full display surface to the screen
 
     def clear_board(self):
         """Clear the board for a fresh render."""
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                self.board[i][j] = None
+        # Clearing logic (if needed)
 
     def place_character(self, character):
-        """Place a character on the board.
+        """Place a character on the screen.
         
         Parameters:
             character (object): The character object to place.
         """
         x, y = character.position
-        self.board[x][y] = character.__class__.__name__[0]
+        # Convert grid position to screen position
+        screen_x = x * self.tile_width
+        screen_y = y * self.tile_width
+        # Example character rendering: simple colored rectangle
+        pygame.draw.rect(self.screen, character.color, (screen_x, screen_y, self.tile_width, self.tile_width))
 
     def display_board(self):
         """Display the board state."""
-        for row in self.board:
-            print(" ".join(cell if cell else "." for cell in row))
+        # Additional rendering logic for the board
